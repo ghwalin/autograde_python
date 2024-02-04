4308b48bb80dd5b706242a097c6e93c9896f4ad0
Marcel Suter <marcel.suter@bzz.ch>
2024-02-04 08:55:56 +0100

    testing stuff

--- py_main.py
+++ py_main.py
@@ -1,10 +1,33 @@
+import os
+
 import requests
 
 from py_lint import py_lint
 from py_test import py_test
 
+def main():
 
-def collect_results() -> None:
+    TARGET_URL = os.environ['TARGET_URL']
+    TOKEN = os.getenv('TOKEN')
+    FUNCTION = os.environ['FUNCTION']
+    ASSIGNMENT = os.environ['ASSIGNMENT']
+    USERNAME = os.environ['USERNAME']
+    LINK = os.environ['LINK']
+
+    print (TARGET_URL, TOKEN, FUNCTION, ASSIGNMENT, USERNAME, LINK)
+
+    result = collect_results()
+    update_moodle(
+        result=result,
+        target_url='https://moodle.it.bzz.ch/moodle',
+        token='aef1c413c539c619c48895e6f6f2d120',
+        function='mod_assignexternal_update_grade',
+        user_name='ghwalin',
+        assignment='lu99-a99-test',
+        external_link='https://github.com/ghwalin/autograde_python'
+    )
+
+def collect_results() -> dict:
     """
     collects the results from all grading modules
     :return:
@@ -25,8 +48,8 @@ def collect_results() -> None:
     result['max'] += testresults['max']
     result['feedback'] += html_out(testresults['feedback'])
 
-    print(result)
-    update_moodle(result)
+    return result
+
 
 
 def html_out(results: dict) -> str:
@@ -52,25 +75,36 @@ def html_out(results: dict) -> str:
     return output
 
 
-def update_moodle(result: dict) -> None:
+def update_moodle(
+        result: dict,
+        target_url: str,
+        token: str,
+        function: str,
+        user_name: str,
+        assignment: str,
+        external_link: str
+) -> None:
     """
     calls the webservice to update the assignment in moodle
     :param result:
+    :param target_url:
+    :param token:
+    :param function:
+    :param user_name:
+    :param assignment:
+    :param external_link:
     :return:
     """
-    # http://localhost/moodle/webservice/rest/server.php
-    moodle_url = 'https://moodle.it.bzz.ch/moodle'
-    token = 'aef1c413c539c619c48895e6f6f2d120'
-    url = f'{moodle_url}/webservice/rest/server.php/?wstoken={token}&wsfunction=mod_assignexternal_update_grade'
+    url = f'{target_url}/webservice/rest/server.php/?wstoken={token}&wsfunction={function}'
     payload = {
-        'assignment_name': 'lu99-a99-test',
-        'user_name': 'ghwalin',
+        'assignment_name': assignment,
+        'user_name': user_name,
         'points': result['points'],
         'max': result['max'],
-        'externallink': 'https://github.com/ghwalin/autograde_python',
+        'externallink': external_link,
         'feedback': result['feedback']
     }
-    response = requests.post(url=url, data=payload)
+    response = requests.post(url=url, data=payload, timeout=30)
     print(response.status_code)
 
 
